92abd28dce3c1dcbddb500d69b3103395764c994
Srikalyan Megati <kalmegati@gmail.com>
2019-08-04 00:57:51 +0000

    Automatically backed up by Learn

--- receipt.rb
+++ receipt.rb
@@ -3,3 +3,11 @@ def register(cart)
     memo += (value[:price] * value[:count])
   }
 end  
+
+cart = {
+  "PEANUT BUTTER" => {:price => 2.40, :clearance => true,  :count => 2},
+  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+  "SOY MILK"     => {:price => 3.60, :clearance => true,  :count => 1}
+}
+
+register(cart)
