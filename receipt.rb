e285f931f77a4cc53667a270ad614767bfdeaf0d
Srikalyan Megati <kalmegati@gmail.com>
2019-08-04 00:58:31 +0000

    Automatically backed up by Learn

--- receipt.rb
+++ receipt.rb
@@ -6,7 +6,7 @@ end
 
 cart = {
   "PEANUT BUTTER" => {:price => 2.40, :clearance => true,  :count => 2},
-  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+  "KALE"         => {:price => 3.00, :clearance => false, :count => 3},
   "SOY MILK"     => {:price => 3.60, :clearance => true,  :count => 1}
 }
 
