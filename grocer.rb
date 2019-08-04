e6a5cf7a6dbd76a241fb109ffacea3e1d8297cbb
Srikalyan Megati <kalmegati@gmail.com>
2019-08-04 00:48:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,50 +1,66 @@
-#def consolidate_cart(cart)
-#  windfall = []
-#  cart.each {|item| windfall.push([(item.keys)[0], item[(item.keys)[0]]])}
-#  p windfall
-#  organized_cart = {}
-#  windfall.length.times { |index|
-#    cart_key = windfall[index]
-#    if organized_cart.keys.include? windfall[index] 
-#      organized_cart[cart_key][:count] += 1
-#    else
-#      organized_cart[cart_key] => windfall[index]
-#      organized_cart[cart_key][:count] => 0
-#    end
-#  }
-#end
+require'pry'
 
-def
-
-#def consolidate_cart(cart)
-#  overcart = {} #creates the hash that the method will return
-#  cart.length.times { |index| #asks to iterate over every hash in the cart array
-#    if overcart.key?(cart[index].keys[0]) # if the product already exists in overcart...
-#      overcart[cart[index].keys[0]][:count] += 1 #...increase the count of the product by one
-#    else #if overcart does not yet contain the product...
-#      overcart[cart[index].keys[0]] = cart[index].keys[0]
-#      overcart[cart[index].keys[0]][:count] = 0
-#    end
-#  }
-#  puts overcart
-#end
+def consolidate_cart(cart)
+  overcart = {}
+  cart.length.times { |index|
+    if overcart.key?(cart[index].keys[0])
+      overcart[cart[index].keys[0]][:count] += 1 
+    else
+      overcart[cart[index].keys[0]] = {
+        price: cart[index][cart[index].keys[0]][:price],
+        clearance: cart[index][cart[index].keys[0]][:clearance]
+      }
+      overcart[cart[index].keys[0]][:count] = 1
+    end
+  }
+  overcart
+end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.length.times { |index|
+    if cart.key?(coupons[index][:item])
+      cart[coupons[index][:item]][:count] -= coupons[index][:num]
+      cart[coupons[index][:item] + " W/COUPON"] = {
+        price: coupons[index][:cost] / coupons[index][:num],
+        clearance: cart[coupons[index][:item]][:clearance],
+        count: coupons[index][:num]
+      }
+    end
+  }
+  cart
 end
+
 
 def apply_clearance(cart)
-  # code here
+  cart.length.times { |index|
+  if cart[cart.keys[index]][:clearance]
+    cart[cart.keys[index]][:price] -= 0.2*cart[cart.keys[index]][:price]
+  end
+  }
+  cart
 end
+
+def receipt(cart)
+  
+end  
+  
 
 def checkout(cart, coupons)
-  # code here
+  cart_2 = consolidate_cart(cart)
+  puts cart_2
+  apply_coupons(cart_2, coupons)
+  puts cart_2
+  apply_clearance(cart_2)
+  puts cart_2
+  
 end
 
-craving = [
+cart = [
   {"AVOCADO" => {:price => 3.00, :clearance => true }},
   {"AVOCADO" => {:price => 3.00, :clearance => true }},
   {"KALE"    => {:price => 3.00, :clearance => false}}
 ]
 
-consolidate_cart(craving)
+coupons = [{:item => "AVOCADO", :num => 2, :cost => 5.00}]
+
+checkout(cart, coupons)
